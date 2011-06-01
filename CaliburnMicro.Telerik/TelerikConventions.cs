38d0e5f6800cdcedff4bf5b9fda777c809b26c1f
Valeriu Caraulean <caraulean@gmail.com>
2011-06-02 00:05:20 +0200

    Initial solution and project structure.
    Added few conventions.
    References: Caliburn.Micro from NuGet, Telerik's controls in lib folder.

---
+++ CaliburnMicro.Telerik/TelerikConventions.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Caliburn.Micro;
+using Telerik.Windows.Controls;
+
+namespace CaliburnMicro.Telerik
+{
+	public static class TelerikConventions
+	{
+		public static void Install()
+		{
+			ConventionManager.AddElementConvention<RadTabControl>(RadTabControl.ItemsSourceProperty,
+			                                                      "ItemsSource",
+			                                                      "SelectionChanged")
+				.ApplyBinding = (viewModelType, path, property, element, convention) =>
+				{
+					if (!ConventionManager.SetBinding(viewModelType, path, property, element, convention))
+						return false;
+
+					var tabControl = (RadTabControl) element;
+					if (tabControl.ContentTemplate == null
+					    && tabControl.ContentTemplateSelector == null
+					    && property.PropertyType.IsGenericType)
+					{
+						var itemType = property.PropertyType.GetGenericArguments().First();
+						if (!itemType.IsValueType && !typeof (string).IsAssignableFrom(itemType))
+							tabControl.ContentTemplate = ConventionManager.DefaultItemTemplate;
+					}
+					ConventionManager.ConfigureSelectedItem(element,
+					                                        RadTabControl.SelectedItemProperty,
+					                                        viewModelType,
+					                                        path);
+
+					if (string.IsNullOrEmpty(tabControl.DisplayMemberPath))
+						ConventionManager.ApplyHeaderTemplate(tabControl,
+						                                      RadTabControl.ItemTemplateProperty,
+						                                      viewModelType);
+					return true;
+				};
+
+			ConventionManager.AddElementConvention<RadMenuItem>(RadMenuItem.ItemsSourceProperty, "DataContext", "Click");
+			ConventionManager.AddElementConvention<RadBusyIndicator>(RadBusyIndicator.IsBusyProperty, "IsBusy", "Loaded");
+		}
+	}
+}
