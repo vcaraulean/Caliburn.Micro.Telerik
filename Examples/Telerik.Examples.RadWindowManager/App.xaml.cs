683904170227e970ce9fa8b2833e40af79b45462
Valeriu Caraulean <caraulean@gmail.com>
2011-06-03 23:44:01 +0200

    Added a sample project to show usage of RadWindowManager.

---
+++ Examples/Telerik.Examples.RadWindowManager/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Telerik.Examples.RadWindowManager
+{
+	public partial class App : Application
+	{
+
+		public App()
+		{
+			InitializeComponent();
+		}
+	}
+}
