e1fd2956fb474cd0e59799c6b62de6e6ae100ba5
Valeriu Caraulean <caraulean@gmail.com>
2012-04-03 16:03:08 +0200

    Updated assembly version to 1.3.5 (postfactum)

--- Caliburn.Micro.Telerik/Properties/AssemblyInfo.cs
+++ Caliburn.Micro.Telerik/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Caliburn.Micro.Telerik")]
-[assembly: AssemblyCopyright("Copyright © Valeriu Caraulean 2011")]
+[assembly: AssemblyCopyright("Copyright © Valeriu Caraulean 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.3.4")]
-[assembly: AssemblyFileVersion("1.3.4")]
+[assembly: AssemblyVersion("1.3.5")]
+[assembly: AssemblyFileVersion("1.3.5")]
