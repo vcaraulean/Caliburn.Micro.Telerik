bd23357e30499a91f19716a05bbf821111f59d18
Valeriu Caraulean <caraulean@gmail.com>
2012-07-18 20:55:32 +0200

    Added a WPF project for testing conventions
    Only basic shell, examples haven't been ported yet

---
+++ WPF/Examples.Conventions/IShell.cs
@@ -0,0 +1,4 @@
+﻿namespace Examples.Conventions
+{
+    public interface IShell {}
+}
---
+++ WPF/Examples.Conventions/ShellViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using Caliburn.Micro;
+
+namespace Examples.Conventions
+{
+	[Export(typeof(IShell))]
+	public class ShellViewModel : Conductor<IScreen>.Collection.OneActive
+	{
+		[ImportMany]
+		public IEnumerable<IScreen> ExportedScreens { get; set; }
+
+		protected override void OnActivate()
+		{
+			base.OnActivate();
+
+			var conventionSamples = ExportedScreens
+				.Where(s => s.GetType().Namespace.StartsWith(typeof(ShellViewModel).Namespace))
+				.OrderBy(s => s.DisplayName);
+			Items.AddRange(conventionSamples);
+		}
+
+	}
+}
