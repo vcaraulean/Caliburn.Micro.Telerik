1a79f1f209a389593b48fed61abce41afc63e3bb
Valeriu Caraulean <caraulean@gmail.com>
2012-08-16 15:01:01 +0200

    Example of using RadContextMenu with Caliburn
    A special trick in XAML is required to get an action executed on
    RadMenuItem's click event.

---
+++ WPF/Examples.Conventions/Implementations/RadContextMenuViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Composition;
+using Caliburn.Micro;
+using Telerik.Windows.Controls;
+
+namespace Examples.Conventions.Implementations
+{
+	[Export(typeof (IScreen))]
+	public class RadContextMenuViewModel : Screen
+	{
+		public RadContextMenuViewModel()
+		{
+			DisplayName = "RadContextMenu";
+		}
+
+		private static void Alert(string message)
+		{
+			RadWindow.Alert(new DialogParameters {Header = "It works", Content = message});
+		}
+
+		public void ExecuteAction1()
+		{
+			Alert("Action 1 executed");
+		}
+
+		public void ExecuteAction2()
+		{
+			Alert("Action 2 executed");
+		}
+
+		public void ExecuteAction3()
+		{
+			Alert("Action 3 executed");
+		}
+	}
+}
